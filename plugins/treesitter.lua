c38f2a9e2dcce2cfe6053e360b783a0d6a8fde06
Varik Matevosyan <varik.matevosyan@coinstats.app>
2023-04-02 13:16:07 +0400

    feat(lsp): added necessary lsp modules

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,19 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    -- ensure_installed = { "lua" },
+    ensure_installed = {
+      "lua",
+      "typescript",
+      "tsx",
+      "bash",
+      "dockerfile",
+      "graphql",
+      "html",
+      "javascript",
+      "python",
+      "rust",
+      "solidity",
+      "sql",
+    },
   },
 }

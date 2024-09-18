097e1d9cd479bac6ce53e5cc5bd9bd8502ae02a8
Narek Galstyan <narekg@berkeley.edu>
2024-09-18 16:36:26 -0700

    Add smart-splits

---
+++ lua/plugins/smart-splits.lua
@@ -0,0 +1,28 @@
+return {
+  "mrjones2014/smart-splits.nvim",
+  event = "VeryLazy", -- load on very lazy for mux detection
+  dependencies = {
+    {
+      "AstroNvim/astrocore",
+      opts = function(_, opts)
+        local maps = opts.mappings
+        maps.n["<C-H>"] = { function() require("smart-splits").move_cursor_left() end, desc = "Move to left split" }
+        maps.n["<C-J>"] = { function() require("smart-splits").move_cursor_down() end, desc = "Move to below split" }
+        maps.n["<C-K>"] = { function() require("smart-splits").move_cursor_up() end, desc = "Move to above split" }
+        maps.n["<C-L>"] = { function() require("smart-splits").move_cursor_right() end, desc = "Move to right split" }
+
+        -- todo:: not working for some reason..
+        maps.n["<M-H>"] = { function() require("smart-splits").resize_up() end, desc = "Resize split up" }
+        maps.n["<M-J>"] = { function() require("smart-splits").resize_down() end, desc = "Resize split down" }
+        maps.n["<M-K>"] = { function() require("smart-splits").resize_left() end, desc = "Resize split left" }
+        maps.n["<M-L>"] = { function() require("smart-splits").resize_right() end, desc = "Resize split right" }
+        vim.notify "smart splits set!"
+      end,
+    },
+  },
+  opts = {
+    ignored_filetypes = { "nofile", "quickfix", "qf", "prompt" },
+    ignored_buftypes = { "nofile" },
+    disable_multiplexer_nav_when_zoomed = false,
+  },
+}

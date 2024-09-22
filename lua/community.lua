f029cfc73f1565762b58550e47a0404ab9831eff
Narek Galstyan <narekg@berkeley.edu>
2024-09-22 15:43:22 -0700

    Add several useful astrocommunity language packs

--- lua/community.lua
+++ lua/community.lua
@@ -14,5 +14,10 @@ return {
   --  NOTE: This pack DOES not add rust_analyzer. This should be managed by rustup.
   --         To add it to rustup, run: rustup component add rust-analyzer
   { import = "astrocommunity.pack.rust" },
+
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.python-ruff" },
+
   -- import/override with your plugins folder
 }
